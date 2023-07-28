8c675055b5409ec7b51bdc0762c88f852c453a13
Joshua Snider <jsnider@mtu.edu>
2023-07-27 22:59:21 -0400

    Empty commit

--- python/asg-scheduler/asg-scheduler.py
+++ python/asg-scheduler/asg-scheduler.py
@@ -46,12 +46,12 @@ metadata_url_for = {
     #   You MUST remove the testing IdP from a production system,
     #   as the testing IdP will allow ANYBODY to log in as ANY USER!
     # WARNING WARNING WARNING
-    'test': 'http://idp.oktadev.com/metadata'
-    }
+    "test": "http://idp.oktadev.com/metadata"
+}
 
 app = Flask(__name__)
 Bootstrap(app)
-app.secret_key = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
+app.secret_key = "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
 login_manager = LoginManager()
 login_manager.setup_app(app)
 logging.basicConfig(level=logging.INFO)
@@ -64,22 +64,17 @@ rv = None
 
 
 def saml_client_for(idp_name=None):
-    '''
+    """
     Given the name of an IdP, return a configuation.
     The configuration is a hash for use by saml2.config.Config
-    '''
+    """
 
     if idp_name not in metadata_url_for:
         raise Exception("Settings for IDP '{}' not found".format(idp_name))
-    acs_url = url_for(
-        "idp_initiated",
-        idp_name=idp_name,
-        _external=True)
+    acs_url = url_for("idp_initiated", idp_name=idp_name, _external=True)
     https_acs_url = url_for(
-        "idp_initiated",
-        idp_name=idp_name,
-        _external=True,
-        _scheme='https')
+        "idp_initiated", idp_name=idp_name, _external=True, _scheme="https"
+    )
 
     #   SAML metadata changes very rarely. On a production system,
     #   this data should be cached as approprate for your production system.
@@ -88,29 +83,29 @@ def saml_client_for(idp_name=None):
         rv = requests.get(metadata_url_for[idp_name])
 
     settings = {
-        'entityid': 'ASGScheduler',
-        'metadata': {
-            'inline': [rv.text],
-            },
-        'service': {
-            'sp': {
-                'endpoints': {
-                    'assertion_consumer_service': [
+        "entityid": "ASGScheduler",
+        "metadata": {
+            "inline": [rv.text],
+        },
+        "service": {
+            "sp": {
+                "endpoints": {
+                    "assertion_consumer_service": [
                         (acs_url, BINDING_HTTP_REDIRECT),
                         (acs_url, BINDING_HTTP_POST),
                         (https_acs_url, BINDING_HTTP_REDIRECT),
-                        (https_acs_url, BINDING_HTTP_POST)
+                        (https_acs_url, BINDING_HTTP_POST),
                     ],
                 },
                 # Don't verify that the incoming requests originate from us via
                 # the built-in cache for authn request ids in pysaml2
-                'allow_unsolicited': True,
+                "allow_unsolicited": True,
                 # Don't sign authn requests, since signed requests only make
                 # sense in a situation where you control both the SP and IdP
-                'authn_requests_signed': False,
-                'logout_requests_signed': True,
-                'want_assertions_signed': True,
-                'want_response_signed': False,
+                "authn_requests_signed": False,
+                "logout_requests_signed": True,
+                "want_assertions_signed": True,
+                "want_response_signed": False,
             },
         },
     }
@@ -130,43 +125,44 @@ class User(UserMixin):
         try:
             user = user_store[user_id]
             self.id = unicode(user_id)
-            self.first_name = user['first_name']
-            self.last_name = user['last_name']
+            self.first_name = user["first_name"]
+            self.last_name = user["last_name"]
         except:
             pass
 
+
 def getEvents():
-    client = boto3.client('autoscaling', region_name='us-east-1')
+    client = boto3.client("autoscaling", region_name="us-east-1")
     return client.describe_scheduled_actions(
-        AutoScalingGroupName='jsnider-mtu-asg',
-        MaxRecords=20
+        AutoScalingGroupName="jsnider-mtu-asg", MaxRecords=20
     )
 
 
 def putEvent(eventTime, eventName, desiredCapacity):
-    client = boto3.client('autoscaling', region_name='us-east-1')
+    client = boto3.client("autoscaling", region_name="us-east-1")
     return client.put_scheduled_update_group_action(
-        AutoScalingGroupName='jsnider-mtu-asg',
+        AutoScalingGroupName="jsnider-mtu-asg",
         ScheduledActionName=eventName,
         StartTime=eventTime,
-        MinSize=int(desiredCapacity)
+        MinSize=int(desiredCapacity),
     )
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User(user_id)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def main_page():
     err = ""
     submittedDateTime = ""
     submittedEventName = ""
-    if request.method == 'POST':
-        formDate = request.form['date']
-        formTime = request.form['time']
-        formEventName = request.form['eventName']
-        desiredCapacity = request.form['desiredCapacity']
+    if request.method == "POST":
+        formDate = request.form["date"]
+        formTime = request.form["time"]
+        formEventName = request.form["eventName"]
+        desiredCapacity = request.form["desiredCapacity"]
         if not formEventName:
             submittedEventName = "Invalid event name"
             err = "Event name is required"
@@ -178,30 +174,39 @@ def main_page():
         elif not formTime:
             submittedDateTime = "Invalid time"
             err = "Time in UTC is required"
-        elif datetime.strptime(formDate + ' ' + formTime, "%Y-%m-%d %H:%M") < datetime.now():
+        elif (
+            datetime.strptime(formDate + " " + formTime, "%Y-%m-%d %H:%M")
+            < datetime.now()
+        ):
             submittedDateTime = "Invalid date and time"
             err = "Date and time cannot be in the past"
         else:
-            submittedDateTime = ''.join(
-                [str(formDate), "T", str(formTime), ":00Z"])
+            submittedDateTime = "".join([str(formDate), "T", str(formTime), ":00Z"])
             submittedEventName = formEventName.replace(" ", "-")
             try:
                 putEvent(submittedDateTime, submittedEventName, desiredCapacity)
             except Exception as e:
                 err = e
     data = getEvents()
-    return render_template('main_page.html', error=err, idp_dict=metadata_url_for, data=data['ScheduledUpdateGroupActions'], submittedDateTime=submittedDateTime, submittedEventName=submittedEventName)
+    return render_template(
+        "main_page.html",
+        error=err,
+        idp_dict=metadata_url_for,
+        data=data["ScheduledUpdateGroupActions"],
+        submittedDateTime=submittedDateTime,
+        submittedEventName=submittedEventName,
+    )
 
 
-@app.route("/saml/sso/<idp_name>", methods=['GET', 'POST'])
+@app.route("/saml/sso/<idp_name>", methods=["GET", "POST"])
 def idp_initiated(idp_name):
-    if request.method == 'GET':
+    if request.method == "GET":
         url = url_for("main_page", _external=True)
         return redirect(url)
     saml_client = saml_client_for(idp_name)
     authn_response = saml_client.parse_authn_request_response(
-        request.form['SAMLResponse'],
-        entity.BINDING_HTTP_POST)
+        request.form["SAMLResponse"], entity.BINDING_HTTP_POST
+    )
     authn_response.get_identity()
     user_info = authn_response.get_subject()
     username = user_info.text
@@ -211,18 +216,18 @@ def idp_initiated(idp_name):
     # isn't in the user store, we create that user first, then log them in
     if username not in user_store:
         user_store[username] = {
-            'first_name': authn_response.ava['FirstName'][0],
-            'last_name': authn_response.ava['LastName'][0],
-            }
+            "first_name": authn_response.ava["FirstName"][0],
+            "last_name": authn_response.ava["LastName"][0],
+        }
     user = User(username)
-    session['saml_attributes'] = authn_response.ava
+    session["saml_attributes"] = authn_response.ava
     login_user(user)
-    url = url_for('user')
+    url = url_for("user")
     # NOTE:
     #   On a production system, the RelayState MUST be checked
     #   to make sure it doesn't contain dangerous URLs!
-    if 'RelayState' in request.form:
-        url = request.form['RelayState']
+    if "RelayState" in request.form:
+        url = request.form["RelayState"]
     return redirect(url)
 
 
@@ -233,8 +238,8 @@ def sp_initiated(idp_name):
 
     redirect_url = None
     # Select the IdP URL to send the AuthN request to
-    for key, value in info['headers']:
-        if key == 'Location':
+    for key, value in info["headers"]:
+        if key == "Location":
             redirect_url = value
     response = redirect(redirect_url, code=302)
     # NOTE:
@@ -244,20 +249,20 @@ def sp_initiated(idp_name):
     #     http://docs.oasis-open.org/security/saml/v2.0/saml-bindings-2.0-os.pdf
     #   We set those headers here as a "belt and suspenders" approach,
     #   since enterprise environments don't always conform to RFCs
-    response.headers['Cache-Control'] = 'no-cache, no-store'
-    response.headers['Pragma'] = 'no-cache'
+    response.headers["Cache-Control"] = "no-cache, no-store"
+    response.headers["Pragma"] = "no-cache"
     return response
 
 
 @app.route("/user")
 @login_required
 def user():
-    return render_template('user.html', session=session)
+    return render_template("user.html", session=session)
 
 
 @app.errorhandler(401)
 def error_unauthorized(error):
-    return render_template('unauthorized.html')
+    return render_template("unauthorized.html")
 
 
 @app.route("/logout")
@@ -266,8 +271,9 @@ def logout():
     logout_user()
     return redirect(url_for("main_page"))
 
+
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get("PORT", 5000))
     if port == 5000:
         app.debug = True
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
--- python/createPasswd.py
+++ python/createPasswd.py
@@ -1,2 +1,8 @@
-import string,random
-print(''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(12)))
+import string, random
+
+print(
+    "".join(
+        random.choice(string.ascii_letters + string.digits + string.punctuation)
+        for _ in range(12)
+    )
+)
